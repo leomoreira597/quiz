512297d36ad5af2f3dbcdc43e1d9b0de4e7bc523
Leonardo Moreira <lmoreira597@gmail.com>
2022-08-10 17:32:51 -0300

    finalizando logica de mostrar resposta

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -1,7 +1,7 @@
 import RespostasModel from "../model/respostas"
 import styles from "../styles/Respostas.module.css"
 
-interface RespostaProps{
+interface RespostaProps {
     valor: RespostasModel
     indice: number
     letra: string
@@ -9,24 +9,37 @@ interface RespostaProps{
     respostaFornecida: (indice: Number) => void
 }
 
-export default function Resposta(props: RespostaProps){
+export default function Resposta(props: RespostaProps) {
     const resposta = props.valor
-    return(
+    return (
         <div className={styles.respostas}
             onClick={() => props.respostaFornecida(props.indice)}>
             <div className={styles.conteudoResposta}>
-                <div className={styles.frente}>
-                    <div className={styles.letra}
-                        style={{ backgroundColor: props.corFundoLetra }}>
-                        {props.letra}
+                {!resposta.revelada ? (
+                    <div className={styles.frente}>
+                        <div className={styles.letra}
+                            style={{ backgroundColor: props.corFundoLetra }}>
+                            {props.letra}
+                        </div>
+                        <div className={styles.valor}>
+                            {resposta.valor}
+                        </div>
                     </div>
-                    <div className={styles.valor}>
-                        {resposta.valor}
+                ) : (
+                    <div className={styles.verso}>
+                        {resposta.certa ? (
+                            <div className={styles.certa}>
+                                <div>A resposta certa é...</div>
+                                <div className={styles.valor}>{resposta.valor}</div>
+                            </div>
+                        ) : (
+                            <div className={styles.errada}>
+                                <div>A resposta informada está errada...</div>
+                                <div className={styles.valor}>{resposta.valor}</div>
+                            </div>
+                        )}
                     </div>
-                </div>
-                <div className={styles.verso}>
-                    
-                </div>
+                )}
             </div>
         </div>
     )
